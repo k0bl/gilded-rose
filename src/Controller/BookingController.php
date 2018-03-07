a1d9668585523708be4e5ca54109a0169e5b4e8d
Cody Scherer <k0bl@outlook.com>
2018-03-07 15:12:11 -0800

    format use statements

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -1,20 +1,20 @@
 <?php
-namespace App\Controller;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\VarDumper\VarDumper;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use FOS\RestBundle\Controller\Annotations as Rest;
-use FOS\RestBundle\View\View;
-use \Datetime;
-use \DateInterval;
 
-use App\Entity\Room;
+namespace App\Controller;
+use \DateInterval;
+use \Datetime;
 use App\Entity\Booking;
 use App\Entity\Cleaning;
 use App\Entity\CleaningCrew;
+use App\Entity\Room;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\VarDumper\VarDumper;
 
 class BookingController extends Controller
 {
@@ -39,9 +39,9 @@ class BookingController extends Controller
      */
     public function postBookingAction(Request $request)
     {
-        /* checkInDate and checkOutDate will be set manually to 4 pm 
-        for check in and 8am for check out. We can expand this in the 
-        future to allow us to accept a checkInDate and checkOutDate 
+        /* checkInDate and checkOutDate will be set manually to 4 pm
+        for check in and 8am for check out. We can expand this in the
+        future to allow us to accept a checkInDate and checkOutDate
         from the request for future reservations.
         */
         $checkInDate = new DateTime();
@@ -53,10 +53,10 @@ class BookingController extends Controller
         //get next available room
         $room = $this->checkRoomAvailability(
             $request->get('luggage_items'), $checkInDate);
-        
+
         //if we cannot find a room, we throw a 400 error
         if (!$room) {
-            throw new HttpException(400, "Cannot complete booking. 
+            throw new HttpException(400, "Cannot complete booking.
                 An error occurred");
         }
         //create new booking
@@ -64,23 +64,23 @@ class BookingController extends Controller
 
         $booking->luggageItems = $request->get('luggage_items');
         $booking->room = $room;
-        
+
         $booking->checkIn = $checkInDate;
         $booking->checkOut = $checkOutDate;
-        
+
         //get the cleaning crew to create a room service request
         $cleaningCrew = $this->getCleaningCrew();
-        
-        //create a cleaning or room service request for the room       
+
+        //create a cleaning or room service request for the room
         $cleaning = $this->getCleaning($room, $cleaningCrew);
-        
-        // increase the occupant count for the cleaning 
+
+        // increase the occupant count for the cleaning
         $cleaning->occupants = $cleaning->occupants + 1;
-        
+
         // update cleaning duration for room, add occupantCleanTime
-        $cleaning->duration = $cleaning->duration + 
+        $cleaning->duration = $cleaning->duration +
             $cleaningCrew->occupantCleanTime;
-        
+
         //get doctrine entity manager
         $em = $this->getDoctrine()->getManager();
         //persist booking and cleaning, flush to database
@@ -89,13 +89,14 @@ class BookingController extends Controller
         $em->flush();
 
         return View::create($room->roomNumber, Response::HTTP_CREATED , []);
-        
+
     }
 
     /* check for incomplete cleaning, or return a new one */
     protected function getCleaning($room, $cleaningCrew)
     {
 
+    }
 
     /* returns most profitable room for guest to stay in */
     protected function checkRoomAvailability($luggage, $checkIn)
@@ -106,5 +107,7 @@ class BookingController extends Controller
     to support multiple cleaning crews in the future */
     protected function getCleaningCrew()
     {
+
+    }
 
 }
