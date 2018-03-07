fe30ae40597c3cae2842f69bd59eb366f0477a77
Cody Scherer <k0bl@outlook.com>
2018-03-07 15:16:24 -0800

    Room entity and repository boilerplate

--- src/Entity/Room.php
+++ src/Entity/Room.php
@@ -5,6 +5,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity
  * @ORM\Table(name="room")
+ * @ORM\Entity(repositoryClass="App\Repository\RoomRepository")
  */
 class Room
 {
@@ -17,21 +18,36 @@ class Room
 
 	/**
 	 * @ORM\Column(type="integer")
-	 */
-	public $maxOccupants
+	*/
+	public $roomNumber;
 
 	/**
 	 * @ORM\Column(type="integer")
 	 */
-	public $totalStorage
+	public $maxOccupants;
 
 	/**
 	 * @ORM\Column(type="integer")
 	 */
-	public $baseCost
+	public $totalStorage;
+
+	/**
+	 * @ORM\Column(type="integer")
+	 */
+	public $baseCost;
     
+	/**
+	 * @ORM\Column(type="integer")
+	 */
+	public $baseStorageCost;
+
     /**
      * @ORM\OneToMany(targetEntity="Booking", mappedBy="room")
      */
     public $bookings;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Cleaning", mappedBy="room")
+     */
+    public $cleanings;
 }
---
+++ src/Repository/RoomRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\NoResultException;
+
+class RoomRepository extends EntityRepository
+{
+
+}
