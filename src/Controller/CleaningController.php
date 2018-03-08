39dc4277cfdadc336d90983675ff8e00b696dfe3
Cody Scherer <k0bl@outlook.com>
2018-03-07 17:20:26 -0800

    patch method to complete cleaning

--- src/Controller/CleaningController.php
+++ src/Controller/CleaningController.php
@@ -1,43 +1,55 @@
 <?php
 namespace App\Controller;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
-use FOS\RestBundle\Controller\Annotations as Rest;
-use FOS\RestBundle\View\View;
-use Symfony\Component\VarDumper\VarDumper;
-use App\Entity\Room;
+
+use \Datetime;
 use App\Entity\Booking;
 use App\Entity\Cleaning;
+use App\Entity\Room;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\VarDumper\VarDumper;
 
 class CleaningController extends Controller
 {
-	/**
-	 * Lists cleanings
-	 * @Rest\Get("/cleaning_schedule")
-	 * @return array
-	 */
-	public function getCleaningAction()
-	{
+    /**
+     * Lists cleanings
+     * @Rest\Get("/cleaning_schedule")
+     * @return array
+     */
+    public function getCleaningAction()
+    {
         $cleaningsRepo = $this->getDoctrine()->getRepository(Cleaning::class);
         $cleanings = $cleaningsRepo->findIncompleteCleanings();
-		return View::create($cleanings, Response::HTTP_OK, []);
-	}
-	/**
-	 * Patch cleaning by id to complete the cleaning
-	 * @Rest\Patch("/cleaning_schedule/{id}")
-	 * @return array
-	 */
-	public function completeCleaningAction($id)
-	{
+        return View::create($cleanings, Response::HTTP_OK, []);
+    }
+    /**
+     * Patch cleaning by id to complete the cleaning
+     * @Rest\Patch("/cleaning_schedule/{id}")
+     * @return array
+     */
+    public function completeCleaningAction(int $id)
+    {
+        VarDumper::dump($id);
         $cleaningsRepo = $this->getDoctrine()->getRepository(Cleaning::class);
-        $cleaning = $cleaningsRepo->findCleaningById($id);
-        $cleaning->completed = new DateTime();
+        $cleaning = $cleaningsRepo->find($id);
+        VarDumper::dump($cleaning);
+        if (!$cleaning) {
+            throw new HttpException(400, "Cleaning does not exist");
+        }
+        if (!$cleaning->completed) {
+            $cleaning->completed = new DateTime();
+        } else {
+            throw new HttpException(400, "Cannot complete cleaning, already completed.");
+        }
 
         $em = $this->getDoctrine()->getManager();
         //persist cleaning, flush to database
         $em->persist($cleaning);
         $em->flush();
 
-		return View::create($cleaning, Response::HTTP_OK, []);
-	}
+        return View::create($cleaning, Response::HTTP_OK, []);
+    }
 }
